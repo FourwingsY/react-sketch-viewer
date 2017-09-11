9a82f9ce2244f10368926886544d8ac5acefe81d
FourwingsY <fourwingsy@gmail.com>
2017-09-11 22:27:06 +0900

    use github pages on master branch

--- scripts/webpack.config.start.js
+++ scripts/webpack.config.start.js
@@ -16,7 +16,7 @@ module.exports = {
 		path: resolve('public'),
 		pathinfo: true,
 		filename: 'dist.[name].js',
-		publicPath: '/',
+		publicPath: '/public',
 	},
 	resolve: {
 		modules: ['node_modules'],
